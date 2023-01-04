9e328a4941b4faa78489b157f7fea619c890d7da
logan <wuyunlonggo@163.com>
2023-01-04 22:10:09 +0800

    [+] 增加 destroyOnClose 属性 避免误解

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <p>点击后1s会切换路由，但是dialog仍然存在</p>
     <t-button @click="onClick">点击触发bug</t-button>
-    <t-dialog :visible.sync="visible" attach="body">
+    <t-dialog :visible.sync="visible" attach="body" destroyOnClose>
       <p>当前路由地址：{{path}}</p>
       <p>ps:路由地址没变化，说明了组件已经被销毁，但是DOM依然存在</p>
     </t-dialog>
